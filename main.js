f77622e6912b9acda2185439bc2b15f79501334a
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-04-23 07:58:16 -0300

    .

--- main.js
+++ main.js
@@ -7,17 +7,9 @@ for(let i = 0; i<botoes.length; i++){
 		for (let j = 0;j<botoes.length; j++){
 			botoes[j].classList.remove('ativo')
 }
-		botoes[i].classList.add('ativo')
+		botoes[i].classList.add('ativo');
+		texto[i].classList.add('ativo')
 	}
 }
 
-const conteudo = document.querySelectorAll(".aba-conteudo")
 
-for(let i = 0; i<conteudo.length; i++){
-	conteudo[i].onclick = function(){
-		for (let j = 0;j<conteudo.length; j++){
-			conteudo[j].classList.remove('ativo')
-}
-		conteudo[i].classList.add('ativo')
-	}
-}
