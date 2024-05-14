d28cd62be7a08abeff064aa46d4e8048b40783a2
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-05-14 07:43:09 -0300

    .

--- main.js
+++ main.js
@@ -18,3 +18,16 @@ const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 let tempoAtual = new Date()
 
 contadores[0].textContent = (tempoObjetivo1 - tempoAtual)/1000/60/60/24
+
+function calculaTempo(tempoObjetivo1){
+
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo1 - tempoAtual;
+let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
+
+return dias;	
+
+}
