e103359c9f2a595be99b8f4b741bcd48c4068065
神様ありがとう <127319080+Team-HErinzo@users.noreply.github.com>
2024-05-15 09:13:44 -0300

    .

--- main.js
+++ main.js
@@ -25,10 +25,10 @@ contadores[1].textContent = (tempoObjetivo2 - tempoAtual)/1000/60/60/24
 contadores[2].textContent = (tempoObjetivo3 - tempoAtual)/1000/60/60/24
 contadores[3].textContent = (tempoObjetivo4 - tempoAtual)/1000/60/60/24
 
-function calculaTempo(tempoObjetivo1){
+function calculaTempo(tempoObjetivo){
 
 let tempoAtual = new Date();
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+let tempoFinal = tempoObjetivo - tempoAtual;
 let segundos = Math.floor(tempoFinal/1000);
 let minutos = Math.floor(segundos/60);
 let horas = Math.floor(minutos/60);
